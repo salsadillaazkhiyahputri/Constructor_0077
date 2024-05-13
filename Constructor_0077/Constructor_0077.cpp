90c96f015359fa8e70704ffb3c6818c99d9b198b
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-05-13 14:51:03 +0700

    implementasi fungsi dari setID

--- Constructor_0077/Constructor_0077.cpp
+++ Constructor_0077/Constructor_0077.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa
-	public:
-		static int main;
-		int id;
-		string nama;
+class mahasiswa {
+public:
+	static int main;
+	int id;
+	string nama;
 
-		void setID();
-		
+	void setID();
+
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama)
+	{
+		setID();
+	}
+};
+
+int mahasiswa::nim = 20;
+
+void mahasiswa::setID()
+{
+	id =ni++; 
+}
+	
+void mahasiswa::printAll()
+
